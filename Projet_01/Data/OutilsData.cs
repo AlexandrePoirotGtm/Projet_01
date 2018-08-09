2f879b5e9c7f69f4ae9a4f04269175a3c5803d8d
George Doggo <volty0@orange.fr>
2018-08-09 16:33:18 +0200

    Listes

--- Projet_01/Data/OutilsData.cs
+++ Projet_01/Data/OutilsData.cs
@@ -8,5 +8,21 @@ namespace Data
 {
 	class OutilsData
 	{
+		const string CheminFichierDest = @"...\Destination.txt";
+		const string CheminFichierVoya = @"...\Voyages.txt";
+		const string CheminFichierDoss = @"...\Dossier.txt";
+		const string CheminFichierCli = @"...\Clients.txt";
+		const string CheminFichierPart = @"...\Participants.txt";
+		const string CheminFichierComm = @"...\Commerciaux.txt";
+
+
+		private List<Destination> destinations;
+		private List<Voyage> voyages;
+		private List<Dossier> dossiers;
+		private List<Client> clients;
+		private List<Participant> partipants;
+		private List<Commerciaux> commerciaux;
+
+
 	}
 }
