fad697ba55f53ff4841fe1dd0cda8a8fae563c24
George Doggo <volty0@orange.fr>
2018-08-09 16:04:27 +0200

    Test Divers et variées

--- Projet_01/Projet_01/Program.cs
+++ Projet_01/Projet_01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 using Metier;
@@ -14,8 +15,6 @@ namespace Application
 		{
 			Console.WriteLine("Hello World!!!");
 			Console.WriteLine("Hello GTM!!!");
-			
-			
 		}
 	}
 }
