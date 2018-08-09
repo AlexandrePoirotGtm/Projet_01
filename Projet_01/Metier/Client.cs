fbe1f5cf7810b04720f7ed603527b030b42c2a3d
George Doggo <volty0@orange.fr>
2018-08-09 15:43:31 +0200

    Implementations

--- Projet_01/Metier/Client.cs
+++ Projet_01/Metier/Client.cs
@@ -8,8 +8,11 @@ namespace Metier
 {
 	public class Client : Personne, IClientèle, ILogger
 	{
-        public int NumeroDeTelephone { get; set; }
-        public string Adresse { get; set; }
-        public bool Civilite { get; set; }
+		public int NumeroDeTelephone { get; set; }
+		public string Adresse { get; set; }
+		public bool Civilite { get; set; }
+		public int NuméroTéléphone { get; set; }
+		public string Pseudo { get; set; }
+		public string MotDePasse { get; set; }
 	}
 }
